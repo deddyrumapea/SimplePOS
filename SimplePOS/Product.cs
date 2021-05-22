26b761904fc7fe266b6bff061e8d83c05b95316e
Deddy Romnan <romnandeddy@gmail.com>
2021-05-22 17:44:01 +0700

    feat: add data models

--- SimplePOS/Product.cs
+++ SimplePOS/Product.cs
@@ -1,25 +1,46 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
 using System.Configuration;
+using System.Data;
 
 namespace SimplePOS
 {
-    class Product
+    internal class Product
     {
-        private string connString = ConfigurationManager.ConnectionStrings["simplepos"].ConnectionString;
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+
+        private string connString;
+        private MySqlConnection conn;
+
+        public Product()
+        {
+            this.connString = ConfigurationManager.ConnectionStrings["simplepos"].ConnectionString;
+            this.conn = new MySqlConnection(connString);
+        }
+
+        public Product(int id, string name, int price, int stock)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.stock = stock;
+            this.connString = ConfigurationManager.ConnectionStrings["simplepos"].ConnectionString;
+            this.conn = new MySqlConnection(connString);
+        }
 
         public string Insert(string name, int price, int stock)
         {
             string result = "";
-            using (MySqlConnection conn = new MySqlConnection(connString))
+            using (conn)
             {
                 conn.Open();
-                using (MySqlCommand cmd = new MySqlCommand("INSERT INTO product (name, price, stock) VALUES (@name, @price, @stock)", conn))
+                string cmdText = "INSERT INTO product (name, price, stock) VALUES (@name, @price, @stock)";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
                 {
                     cmd.Parameters.AddWithValue("@name", name);
                     cmd.Parameters.AddWithValue("@price", price);
@@ -28,12 +49,14 @@ namespace SimplePOS
                     try
                     {
                         cmd.ExecuteNonQuery();
-                        conn.Close();
-                    }catch(Exception e)
+                    }
+                    catch (Exception e)
                     {
                         result = e.Message;
                     }
                 }
+
+                conn.Close();
             }
             return result;
         }
@@ -41,16 +64,190 @@ namespace SimplePOS
         public DataTable FindAll()
         {
             DataTable dataTable = new DataTable();
-            using (MySqlConnection conn = new MySqlConnection(connString))
+
+            using (conn)
             {
-                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM product", conn))
+                conn.Open();
+                string cmdText = "SELECT * FROM product";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
                 {
-                    conn.Open();
                     MySqlDataReader dataReader = cmd.ExecuteReader();
                     dataTable.Load(dataReader);
                 }
+
+                conn.Close();
             }
             return dataTable;
+        }
+
+        public Product Find(int id)
+        {
+            Product product = null;
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT * FROM product WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    using (dataReader)
+                    {
+                        while (dataReader.Read())
+                        {
+                            string name = dataReader.GetString(1);
+                            int price = dataReader.GetInt32(2);
+                            int stock = dataReader.GetInt32(3);
+                            product = new Product(id, name, price, stock);
+                        }
+                        dataReader.Close();
+                    }
+
+                }
+                conn.Close();
+            }
+            return product;
+        }
+
+        public string Update(int id, string name, int price, int stock)
+        {
+            string result = "";
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "UPDATE product SET name=@name, price=@price, stock=@stock WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@price", price);
+                    cmd.Parameters.AddWithValue("@stock", stock);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+
+                conn.Close();
+            }
+            return result;
+        }
+
+        public string Delete(int id)
+        {
+            string result = "";
+
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "DELETE FROM product WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+
+                conn.Close();
+            }
+            return result;
+        }
+
+        public string InsertToTransaction(int transactionId, int productId, int quantity)
+        {
+            string result = "";
+
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "INSERT INTO transaction_product (id_transaction, id_product, quantity) VALUES (@id_transaction, @id_product, @quantity)";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id_transaction", transactionId);
+                    cmd.Parameters.AddWithValue("@id_product", productId);
+                    cmd.Parameters.AddWithValue("@quantity", quantity);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+                conn.Close();
+            }
+
+            return result;
+        }
+
+        public DataTable FindFromTransaction(int transactionId)
+        {
+            DataTable dataTable = new DataTable();
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT id_product AS 'id', name, price, quantity, price*quantity AS subtotal " +
+                    "FROM product, transaction_product " +
+                    "WHERE transaction_product.id_product = product.id " +
+                    "AND transaction_product.id_transaction = @transactionId";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@transactionId", transactionId);
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    dataTable.Load(dataReader);
+                }
+                conn.Close();
+            }
+            return dataTable;
+        }
+
+        public int GenerateFreshId()
+        {
+            int id = 0;
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'simplepos' AND TABLE_NAME = 'product'";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    dataReader.Read();
+                    id += dataReader.GetInt32(0);
+                    dataReader.Close();
+                }
+
+                conn.Close();
+            }
+            return id;
         }
     }
 }
---
+++ SimplePOS/Transaction.cs
@@ -0,0 +1,206 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePOS
+{
+    class Transaction
+    {
+        public int id { get; set; }
+        public string date { get; set; }
+        public int total { get; set; }
+        public int cash { get; set; }
+        public int change { get; set; }
+        private string connString;
+        private MySqlConnection conn;
+
+        public Transaction()
+        {
+            this.connString = ConfigurationManager.ConnectionStrings["simplepos"].ConnectionString;
+            this.conn = new MySqlConnection(connString);
+        }
+
+        public Transaction(int id, string date, int total, int cash, int change)
+        {
+            this.id = id;
+            this.date = date;
+            this.total = total;
+            this.cash = cash;
+            this.change = change;
+            this.connString = ConfigurationManager.ConnectionStrings["simplepos"].ConnectionString;
+            this.conn = new MySqlConnection(connString);
+        }
+
+        public string Insert(string date, int total, int cash, int change)
+        {
+            string result = "";
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "INSERT INTO transaction (date, total, cash, `change`) VALUES (@date, @total, @cash, @change)";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@date", date);
+                    cmd.Parameters.AddWithValue("@total", total);
+                    cmd.Parameters.AddWithValue("@cash", cash);
+                    cmd.Parameters.AddWithValue("@change", change);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+
+                conn.Close();
+            }
+            return result;
+        }
+
+        public DataTable FindAll()
+        {
+            DataTable dataTable = new DataTable();
+
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT id, date, total FROM transaction";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    dataTable.Load(dataReader);
+                }
+
+                conn.Close();
+            }
+            return dataTable;
+        }
+
+        public Transaction Find(int id)
+        {
+            Transaction transaction = null;
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT * FROM transaction WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    using (dataReader)
+                    {
+                        while (dataReader.Read())
+                        {
+                            string date = dataReader.GetString(1);
+                            int total = dataReader.GetInt32(2);
+                            int cash = dataReader.GetInt32(3);
+                            int change = dataReader.GetInt32(4);
+                            transaction = new Transaction(id, date, total, cash, change);
+                        }
+                        dataReader.Close();
+                    }
+                }
+                conn.Close();
+            }
+            return transaction;
+        }
+
+        public string Update(Transaction transaction)
+        {
+            string result = "";
+
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "UPDATE transaction SET date=@date, total=@total, cash=@cash, `change`=@change WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", transaction.id);
+                    cmd.Parameters.AddWithValue("@date", transaction.date);
+                    cmd.Parameters.AddWithValue("@total", transaction.total);
+                    cmd.Parameters.AddWithValue("@cash", transaction.cash);
+                    cmd.Parameters.AddWithValue("@change", transaction.change);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+                conn.Close();
+            }
+
+            return result;
+        }
+
+        public string Delete(int id)
+        {
+            string result = "";
+
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "DELETE FROM transaction WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        result = e.Message;
+                    }
+                }
+                conn.Close();
+            }
+            return result;
+        }
+
+        public int LastInsertedId()
+        {
+            int result = 0;
+            using (conn)
+            {
+                conn.Open();
+                string cmdText = "SELECT MAX(id) FROM transaction";
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+
+                using (cmd)
+                {
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+                    dataReader.Read();
+                    result = dataReader.GetInt32(0);
+                    dataReader.Close();
+                }
+
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
